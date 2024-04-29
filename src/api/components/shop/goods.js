2e0a71d0262c43f06acadfe5f8ae24509906af54
drewjohnjaya <andrewjohnsonwijaya>
2024-04-29 19:12:42 +0700

    add files for goods

---
+++ src/api/components/shop/goods.js
@@ -0,0 +1,17 @@
+[
+  {
+    "name": "Fan",
+    "price": "$10",
+    "stock": "100"
+  },
+  {
+    "name": "Laptop",
+    "price": "$20",
+    "stock": "200"
+  },
+  {
+    "name": "Computer",
+    "price": "$30",
+    "stock": "300"
+  }
+]
